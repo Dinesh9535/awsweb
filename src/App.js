42c009782110c9efe36b3a112f922738690b507a
Dinesh S <dinesh.s@valtech.com>
2024-02-06 21:46:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExdTdoczBwd3puYnplamllYTU1Nml3cjNjdjdzZDE2b3h2N3ZxNmV5cSZlcD12MV9naWZzX3RyZW5kaW5nJmN0PWc/3LLLtPB8ycjGmTTVKa/giphy.gif"/>
+      <div><p>Hurray!!!     i  am  in  EC2</p></div>
     </div>
   );
 }
